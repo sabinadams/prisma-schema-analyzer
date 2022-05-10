2d4fd3736da43761b3d4addb185fec195a0c0cce
Sabin Adams <sabintheworld@gmail.com>
2022-05-10 08:35:18 -0700

    Adds error details

--- app/routes/dmmf.ts
+++ app/routes/dmmf.ts
@@ -8,18 +8,22 @@ export const action: ActionFunction = async ({ request }) => {
 
   try {
     await formatSchema({ schema });
-  } catch (e) {
-    return json({ error: "Invalid schema." }, { status: 400 });
+  } catch (e: any) {
+    return json(
+      { error: "Invalid schema.", details: e.message },
+      { status: 400 }
+    );
   }
 
   let dmmf = {};
   try {
     dmmf = await getDMMF({ datamodel: schema });
-  } catch (e) {
+  } catch (e: any) {
     return json(
       {
         error:
           "There was a problem parsing the DMMF object. Please check your schema.",
+        details: e.message,
       },
       { status: 400 }
     );
